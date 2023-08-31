6c54cb70f5bd3b952dc407d26595d4f3901a7282
EdiGaliani <edigaliano@gmail.com>
2023-08-31 18:07:27 -0300

    Renderization of the List in Component

--- 3_avancando/src/App.js
+++ 3_avancando/src/App.js
@@ -7,6 +7,12 @@ import ManageData from './components/ManageData';
 import ShowUserName from './components/ShowUserName';
 
 function App() {
+  const Cars = [
+    {id:1, brand: "Ferrari", km: 0, color: "Amarelo", newCar: true},
+    {id:2, brand: "Renault", km: 12548, color: "Verde", newCar: false},
+    {id:3, brand: "Fiat", km: 0, color: "Preto", newCar: true}
+  ]
+
   return (
     <div className="App">
       <h1>Avançando em React</h1>
@@ -24,7 +30,18 @@ function App() {
       <ListRender />
       <CondicionalRender />
       <ShowUserName name="Edi"/>
-      <CarDetails brand="Fiat" km={120000} color="Branco"/>
+      <CarDetails brand="Fiat" km={120000} color="Branco" newCar={true}/>
+      <CarDetails brand="VW" km={138000} color="Preto" newCar={false}/>
+      <CarDetails brand="Ford" km={18000} color="Prata" newCar={true}/>
+      {/* Renderizando Lista em componentes */}
+      {Cars.map((car) => (
+        <CarDetails 
+          brand={car.brand} 
+          km={car.km} 
+          color={car.color}
+          newCar={car.newCar} 
+        />
+      ))}
     </div>
   );
 }
