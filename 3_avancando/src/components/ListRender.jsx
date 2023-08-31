93bb6624cadbd446ecc3c5b8e62dbacb37a53033
EdiGaliani <edigaliano@gmail.com>
2023-08-31 12:24:42 -0300

    Learning about Previous State

--- 3_avancando/src/components/ListRender.jsx
+++ 3_avancando/src/components/ListRender.jsx
@@ -2,13 +2,23 @@ import React, { useState } from 'react'
 
 const ListRender = () => {
     const [List] = useState(["Matheus", "Pedro", "Josias", "Maria"]);
-    const [users] = useState([
+    const [users, setUsers] = useState([
         {id: 1, name: "Matheus", age: 36},
         {id: 2, name: "Pedro", age: 58},
         {id: 3, name: "Josias", age: 14},
         {id: 4, name: "Maria", age: 24}
 
     ])
+
+    const deleteRandom = () => {
+        const randomNumber = Math.floor(Math.random() * 5);
+        console.log(randomNumber)
+
+        setUsers((prevUsers) => {
+            console.log(prevUsers)
+            return prevUsers.filter((user) => randomNumber !== user.id);
+        })
+    }
   return (
     <div>
         <ul>
@@ -23,8 +33,10 @@ const ListRender = () => {
                 </li>
             ))}
         </ul>
+        <button onClick={deleteRandom}>Delete Random User</button>
     </div>
   )
+  
 }
 
 export default ListRender
