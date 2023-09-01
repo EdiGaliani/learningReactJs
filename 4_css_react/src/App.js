0663d3456fbcccdf63025377e3a5b84ba551fa53
EdiGaliani <edigaliano@gmail.com>
2023-09-01 15:53:22 -0300

    Dynamic CSS

--- 4_css_react/src/App.js
+++ 4_css_react/src/App.js
@@ -1,22 +1,29 @@
+import { useState } from 'react';
 import './App.css';
 import MyComponent from './components/MyComponent';
 
 function App() {
   const n = 15;
-  const name = "Edi";
+  const [name] = useState("Edi");
+  const redTitle = true;
+
+
   return (
     <div className="App">
-      {/* CSS global */}
+      {/* CSS Global */}
       <h1>React com CSS</h1>
-      {/* CSS de componente */}
+      
+      {/* CSS de Componente */}
       <MyComponent />
-      <p>Este parágafro é do App.js</p>
+      <p>Este parágafro é do App.js</p> 
+      
       {/* Inline CSS */}
       <p style={{
         color: "magenta",
         padding: "25px",
         borderTop: "2px solid red"
       }}>Este elemento foi estilizado de forma inline</p>
+      
       {/* CSS Inline Dinâmico */}
       <h2 style={n < 10 ? {color:"purple"} : {color:"pink"}}>CSS dinâmico</h2>
       <h2 style={n > 10 ? {color:"purple"} : {color:"pink"}}>CSS dinâmico</h2>
@@ -24,6 +31,10 @@ function App() {
         ? {color:"green", backgroundColor:"#000"} 
         : null
       }>Teste Nome</h2>
+
+      {/* Classe Dinâmica */}
+      <h2 className={redTitle ? "red-title" : "title"}>Este título vai ter classe dinâmica</h2>
+
     </div>
   );
 }
