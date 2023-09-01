31a2f68b14a75c8bb6222b40b619c6ae83298b90
EdiGaliani <edigaliano@gmail.com>
2023-09-01 15:39:08 -0300

    CSS Inline and Dynamic

--- 4_css_react/src/App.js
+++ 4_css_react/src/App.js
@@ -2,6 +2,8 @@ import './App.css';
 import MyComponent from './components/MyComponent';
 
 function App() {
+  const n = 15;
+  const name = "Edi";
   return (
     <div className="App">
       {/* CSS global */}
@@ -9,6 +11,19 @@ function App() {
       {/* CSS de componente */}
       <MyComponent />
       <p>Este parágafro é do App.js</p>
+      {/* Inline CSS */}
+      <p style={{
+        color: "magenta",
+        padding: "25px",
+        borderTop: "2px solid red"
+      }}>Este elemento foi estilizado de forma inline</p>
+      {/* CSS Inline Dinâmico */}
+      <h2 style={n < 10 ? {color:"purple"} : {color:"pink"}}>CSS dinâmico</h2>
+      <h2 style={n > 10 ? {color:"purple"} : {color:"pink"}}>CSS dinâmico</h2>
+      <h2 style={name === "Edi" 
+        ? {color:"green", backgroundColor:"#000"} 
+        : null
+      }>Teste Nome</h2>
     </div>
   );
 }
