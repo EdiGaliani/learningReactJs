fb328e0b93b03335a592660f9539d61ccd90ad2d
EdiGaliani <edigaliano@gmail.com>
2023-09-04 16:44:53 -0300

    Introduction to the Forms in React

---
+++ 5_form_em_react/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import MyForm from './components/MyForm';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Forms</h1>
+      <MyForm />
+    </div>
+  );
+}
+
+export default App;
