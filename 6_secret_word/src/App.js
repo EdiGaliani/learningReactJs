5c2a987d62663de7c03f53a4f20f3b4bd2fc2840
EdiGaliani <edigaliano@gmail.com>
2023-09-06 18:18:51 -0300

    Creating Game components

--- 6_secret_word/src/App.js
+++ 6_secret_word/src/App.js
@@ -1,12 +1,36 @@
+// CSS
 import './App.css';
+
+// React
+
+// Data
+import { wordsList } from './data/words';
+import { useState } from 'react';
+
+// Components
 import StartScreen from './components/StartScreen';
+import Game from './components/Game';
+import GameOver from './components/GameOver';
+
+
+const stages = [
+  { id: 1, name: "start"},
+  { id: 2, name: "game"},
+  { id: 3, name: "end"}
+]
 
 function App() {
+  const [gameStage, setGameStage] = useState(stages[0].name)
+  const [words] = useState(wordsList)
+
   return (
     <div className="App">
-      < StartScreen />
+      {gameStage === "start" && <StartScreen />}
+      {gameStage === "game" && <Game />}
+      {gameStage === "end" && <GameOver />}
     </div>
   );
 }
 
 export default App;
+    
